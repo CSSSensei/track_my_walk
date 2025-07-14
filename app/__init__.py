dc3357fcc6b8a164cdd327652f924394f6be8fe3
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-14 05:04:25 +0300

    add custom 404 not found page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.extensions.database import close_db_interface, init_db_command
 from config import Config
 from app.extensions import database
@@ -17,5 +17,9 @@ def create_app():
     app.register_blueprint(main.bp)
     app.register_blueprint(admin.bp)
     app.register_blueprint(api.bp)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
 
     return app
