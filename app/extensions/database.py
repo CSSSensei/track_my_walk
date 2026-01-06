2edc0c3469d0b74fed5a057a5af52ef2803b9bb6
nikm67 <mihail.nikolenk0@yandex.ru>
2026-01-06 04:48:36 +0300

    refactor: remove sqlite database interface

--- app/extensions/database.py
+++ app/extensions/database.py
@@ -1,55 +1,31 @@
+import click
+from flask import g
+from flask.cli import with_appcontext
+
 from app.extensions.db_interface import DBInterface
-from app.extensions.sqlite_db import SQLiteDB
 from app.extensions.postgres import PostgresDB
-from flask import g, current_app
 
 
 def get_db_interface() -> DBInterface:
-    """
-    Provides a database interface instance (e.g., SQLiteDB or PostgresDB) for the current request.
-    Stores it in Flask's `g` object to reuse within the same request.
-    """
-    if 'db_interface' not in g:
-        db_type = current_app.config.get('DATABASE_TYPE', 'postgres')
-        if db_type == 'sqlite':
-            db_instance = SQLiteDB()
-        elif db_type == 'postgres':
-            db_instance = PostgresDB()
-        else:
-            raise ValueError(f"Unknown database type: {db_type}")
-
-        g.db_interface = db_instance
-        g.db_interface.connect()
+    if "db_interface" not in g:
+        g.db_interface = PostgresDB()
     return g.db_interface
 
 
 def close_db_interface(e=None):
-    """
-    Closes the database interface connection at the end of a request.
-    """
-    db_interface = g.pop('db_interface', None)
+    db_interface = g.pop("db_interface", None)
     if db_interface is not None:
         db_interface.close()
 
 
+@click.command("init-db")
+@with_appcontext
+def init_db_command():
+    db = PostgresDB()
+    db.init_db()
+    click.echo("Initialized the postgres database.")
+
+
 def init_app(app):
     app.teardown_appcontext(close_db_interface)
-    app.cli.add_command(app.cli.command('init-db')(init_db_command))
-
-
-def init_db_command(app):
-    """Clear existing data and create new tables."""
-    with app.app_context():
-        # Get an instance of the appropriate DB and initialize it
-        db_type = app.config.get('DATABASE_TYPE', 'postgres')
-        if db_type == 'sqlite':
-            db_interface = SQLiteDB()
-        elif db_type == 'postgres':
-            db_interface = PostgresDB()
-        else:
-            raise ValueError(f"Unknown database type: {db_type}")
-
-        db_interface.init_db()
-        # No need to explicitly close here as it's a short-lived instance,
-        # could add db_interface.close() if preferred for clarity.
-        print(f'Initialized the {db_type} database.')
+    app.cli.add_command(init_db_command)
